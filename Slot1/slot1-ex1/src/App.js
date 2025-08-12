12356289c729693ba984d0bf0ca63b338e642571
HoangBao19 <Baoroyal2111@gmail.com>
2025-08-12 09:12:47 +0700

    Remove submodule slot1-ex1 and add as normal folder

---
+++ Slot1/slot1-ex1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  const myName = "Hoang Gia Bao"; 
+
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>My name is  {myName}</p>
+    </div>
+  );
+}
+
+export default App;
